d45be5577a98db46d90f5f41e7ec89a27ad23f59
Jakub Jirgl <jirgl.jakub@gmail.com>
2016-08-03 17:19:48 +0200

    Add linear gradient at getting contrast color

--- src/getters.ts
+++ src/getters.ts
@@ -1,14 +1,36 @@
 import { rgb } from './colorModels';
 import * as converters from './converters';
 
-const threshold = 186;
-const lightColor = { r: 255, g: 255, b: 255 };
-const darkColor = { r: 0, g: 0, b: 0 };
+const contrastColorThreshold = 186;
+const contrastColorLinearRange = 10;
+const whiteColor = { r: 255, g: 255, b: 255 };
+const blackColor = { r: 0, g: 0, b: 0 };
 
-export function getContrastColorFromRgb(rgb: rgb): rgb {
-    return converters.rgbToGray(rgb) > threshold ? darkColor : lightColor;
+export enum GradientType {
+    Step, Linear
 }
 
-export function getContrastColorFromGray(gray: number): rgb {
-    return gray > threshold ? darkColor : lightColor;
+function getLinearContrastColor(gray: number): rgb {
+    const min = contrastColorThreshold - contrastColorLinearRange / 2;
+    const max = contrastColorThreshold + contrastColorLinearRange / 2;
+
+    if (gray > max) return blackColor;
+    if (gray < min) return whiteColor;
+
+    const ratio = 1 - (gray - min) / contrastColorLinearRange;
+    const gradientGray = Math.round(255 * ratio);
+    return { r: gradientGray, g: gradientGray, b: gradientGray };
+}
+
+export function getContrastColorFromRgb(rgb: rgb, type: GradientType): rgb {
+    const gray = converters.rgbToGray(rgb);
+    if (type === GradientType.Step) return gray > contrastColorThreshold ? blackColor : whiteColor;
+
+    return getLinearContrastColor(gray);
+}
+
+export function getContrastColorFromGray(gray: number, type: GradientType): rgb {
+    if (type === GradientType.Step) return gray > contrastColorThreshold ? blackColor : whiteColor;
+
+    return getLinearContrastColor(gray);
 }
